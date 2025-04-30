38065186f65c13e62f2d0fb40be4b4a992faf38d
StaTicDKK17 <tobse2002@gmail.com>
2025-04-30 21:29:21 +0200

    WORKING LEXER AND PARSER

---
+++ Mango/Mango/AbSyn.fs
@@ -0,0 +1,40 @@
+module AbSyn
+
+(*** Helper Functions ***)
+let toCString (s : string) : string =
+    let escape c =
+        match c with
+            | '\\' -> "\\\\"
+            | '"'  -> "\\\""
+            | '\n' -> "\\n"
+            | '\t' -> "\\t"
+            | _    -> System.String.Concat [c]
+    String.collect escape s
+
+// Doesn't actually support all escapes.  Too badefacilliteter.
+let fromCString (s : string) : string =
+    let rec unescape l: char list =
+        match l with
+            | []                -> []
+            | '\\' :: 'n' :: l' -> '\n' :: unescape l'
+            | '\\' :: 't' :: l' -> '\t' :: unescape l'
+            | '\\' :: c   :: l' -> c    :: unescape l'
+            | c           :: l' -> c    :: unescape l'
+    Seq.toList s |> unescape |> System.String.Concat
+
+(* position: (line, column) *)
+type Position = int * int
+
+let rec ppType = function
+  | Int      -> "int"
+  | Char     -> "char"
+  | Bool     -> "bool"
+
+
+type Exp =
+    Constant of int * Position
+    | StringLit of string * Position
+    | Var   of string * Position
+    | Window of string * int * int * Position
+
+type Prog = Exp list
--- Mango/Mango/Program.fs
+++ Mango/Mango/Program.fs
@@ -1,4 +1,4 @@
-﻿namespace CounterApp
+﻿module Program
 
 open Avalonia
 open Avalonia.Controls.ApplicationLifetimes
@@ -8,6 +8,75 @@ open Avalonia.Controls
 open Avalonia.FuncUI
 open Avalonia.FuncUI.DSL
 open Avalonia.Layout
+open System.IO
+open Parser
+open System
+open FSharp.Text.Lexing
+open System.Text
+
+exception SyntaxError of int * int
+
+let printPos (errString : string) : unit =
+    let rec state3 (s : string) (p : int) (lin : string) (col : int) =
+        (* read digits until not *)
+        let c = s.[p]
+        if System.Char.IsDigit c
+        then state3 s (p-1) (System.Char.ToString c + lin) col
+        else raise (SyntaxError (System.Int32.Parse lin, col))
+
+    let rec state2 (s : string) (p : int) (col : string) =
+        (* skip from position until digit *)
+        let c = s.[p]
+        if System.Char.IsDigit c
+        then state3 s (p-1) (System.Char.ToString c) (System.Int32.Parse col)
+        else state2 s (p-1) col
+
+    let rec state1 (s : string) (p : int) (col : string) =
+        (* read digits until not *)
+        let c = s.[p]
+        if System.Char.IsDigit c
+        then state1 s (p-1) (System.Char.ToString c + col)
+        else state2 s (p-1) col
+
+    let rec state0 (s : string) (p : int) =
+        (* skip from end until digit *)
+        let c = s.[p]
+        if System.Char.IsDigit c
+        then state1 s (p-1) (System.Char.ToString c)
+        else state0 s (p-1)
+
+    state0 errString (String.length errString - 1)
+
+// Parse program from string.
+let parseString (s : string) : AbSyn.Prog =
+    Parser.Prog Lexer.Token
+    <| LexBuffer<_>.FromBytes (Encoding.UTF8.GetBytes s)
+
+let parseFastoFile (filename : string) : AbSyn.Prog =
+  let txt = try  // read text from file given as parameter with added extension
+              let inStream = File.OpenText (filename + ".fo")
+              let txt = inStream.ReadToEnd()
+              inStream.Close()
+              txt
+            with  // or return empty string
+              | ex -> ""
+  if txt <> "" then // valid file content
+    let program =
+      try
+        parseString txt
+      with
+        | Lexer.LexicalError (info,(line,col)) ->
+            printfn "%s at line %d, position %d\n" info line col
+            System.Environment.Exit 1
+            []
+        | ex ->
+            if ex.Message = "parse error"
+            then printPos Parser.ErrorContextDescriptor
+            else printfn "%s" ex.Message
+            System.Environment.Exit 1
+            []
+    program
+  else failwith "Invalid file name or empty file"
 
 module Main =
 
