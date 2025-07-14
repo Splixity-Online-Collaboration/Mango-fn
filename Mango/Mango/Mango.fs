fc781223b3a0209118c9315dc843e104719b8cdd
Thobias Larsen <Thobias.Larsen@proton.me>
2025-07-14 19:07:41 +0200

    Refactor Mango to use smaller maintainable modules

--- Mango/Mango/Mango.fs
+++ Mango/Mango/Mango.fs
@@ -1,106 +1,67 @@
-﻿module Program
+﻿module Globals =
+    let filepath = ref None
 
-open Avalonia
-open Avalonia.Controls.ApplicationLifetimes
-open Avalonia.Themes.Fluent
-open Avalonia.FuncUI.Hosts
-open System.IO
-open FSharp.Text.Lexing
-open System.Text
-open Interpreter
+module FileIO =
 
-let mutable filepath = ""
+    open System.IO
 
-exception SyntaxError of int * int
+    let readContent path =
+        try
+            File.ReadAllText(path) |> Ok
+        with ex -> Error $"Could not read file: {ex.Message}"
 
-let printPos (errString : string) : unit =
-    let rec state3 (s : string) (p : int) (lin : string) (col : int) =
-        (* read digits until not *)
-        let c = s.[p]
-        if System.Char.IsDigit c
-        then state3 s (p-1) (System.Char.ToString c + lin) col
-        else raise (SyntaxError (System.Int32.Parse lin, col))
+module ParserWrapper =
 
-    let rec state2 (s : string) (p : int) (col : string) =
-        (* skip from position until digit *)
-        let c = s.[p]
-        if System.Char.IsDigit c
-        then state3 s (p-1) (System.Char.ToString c) (System.Int32.Parse col)
-        else state2 s (p-1) col
+    open System.Text
+    open FSharp.Text.Lexing
 
-    let rec state1 (s : string) (p : int) (col : string) =
-        (* read digits until not *)
-        let c = s.[p]
-        if System.Char.IsDigit c
-        then state1 s (p-1) (System.Char.ToString c + col)
-        else state2 s (p-1) col
+    let parseString (source: string) =
+        try
+            let lexbuf = LexBuffer<_>.FromBytes(Encoding.UTF8.GetBytes(source))
+            Ok (Parser.Prog Lexer.Token lexbuf)
+        with
+        | Lexer.LexicalError (msg, (line, col)) ->
+            Error $"Lexical error: {msg} at line {line}, column {col}"
+        | ex -> Error $"Parser error: {ex.Message}"
 
-    let rec state0 (s : string) (p : int) =
-        (* skip from end until digit *)
-        let c = s.[p]
-        if System.Char.IsDigit c
-        then state1 s (p-1) (System.Char.ToString c)
-        else state0 s (p-1)
+module AppMain =
 
-    state0 errString (String.length errString - 1)
+    open Avalonia
+    open Avalonia.Controls.ApplicationLifetimes
+    open Avalonia.FuncUI.Hosts
+    open Avalonia.Themes.Fluent
+    open FileIO
+    open ParserWrapper
+    open Interpreter
 
-/// <summary> Lexes and parses a mango file</summary>
-/// <param name="s">string representing mango file</param>
-/// <returns>Abstract Syntax tree or error message</returns>
-let parseString (s : string) =
-    try 
-        Ok (Parser.Prog Lexer.Token <| LexBuffer<_>.FromBytes (Encoding.UTF8.GetBytes s))
-    with 
-        | Lexer.LexicalError (info,(line,col)) ->
-            Error (sprintf "%s at line %d, position %d\n" info line col)
-        | ex -> Error ex.Message
+    let mutable filepath = "examples/window.mango"
 
-/// <summary>Read the file at the path given as parameter and return the result</summary>
-/// <param name="path">Path to mango program</param>
-/// <returns>Result containing string of the read file or IO error</returns>
-let readContent path = 
-    try // read text from file given as parameter with added extension
-        let inStream = File.OpenText path
-        let txt = inStream.ReadToEnd()
-        inStream.Close()
-        Ok txt
-    with // or return the exception
-        | ex -> Error ex
+    type App() =
+        inherit Application()
+        override this.Initialize() =
+            this.Styles.Add(FluentTheme())
 
-/// <summary>Open file and perform lexing and parsing on the mango file</summary>
-/// <param name="filename">Path to mango file</param>
-/// <returns> The abstract syntax tree or an error message</returns>
-let parseMangoFile (filename : string) =
-  let txt = readContent filename
-  match txt with
-  | Ok content -> parseString content
-  | Error error -> Error error.Message
-
-type App() =
-    inherit Application()
-
-    override this.Initialize() =
-        this.Styles.Add (FluentTheme())
-
-    override this.OnFrameworkInitializationCompleted() =
-        let absyn = parseMangoFile filepath
-
-        match absyn with
-        | Ok syntax_tree ->
-            match this.ApplicationLifetime with
-            | :? IClassicDesktopStyleApplicationLifetime as desktopLifetime ->
-                desktopLifetime.MainWindow <- interpret (new HostWindow()) syntax_tree
-            | _ -> ()
-        | Error message -> failwith message
+        override this.OnFrameworkInitializationCompleted() =
+            match readContent filepath with
+            | Error err -> failwithf "File error: %s" err
+            | Ok source ->
+                match parseString source with
+                | Error msg -> failwithf "Parse error: %s" msg
+                | Ok syntax_tree ->
+                    match this.ApplicationLifetime with
+                    | :? IClassicDesktopStyleApplicationLifetime as desktop ->
+                        desktop.MainWindow <- interpret (HostWindow()) syntax_tree
+                    | _ -> ()
 
 module Program =
 
+    open Avalonia
+    open AppMain
+
     [<EntryPoint>]
-    let main(args: string[]) =
-        if args.Length = 0 then
-            do filepath <- "examples/window.mango"
-        else
-            do filepath <- args[0]
+    let main (args: string[]) =
+        let path = if args.Length > 0 then args[0] else "examples/test.mango"
+        Globals.filepath.Value <- Some path
         AppBuilder
             .Configure<App>()
             .UsePlatformDetect()
