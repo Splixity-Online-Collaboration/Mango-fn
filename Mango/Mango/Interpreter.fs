eaa4ecce037d9d638fa7d824f8462efca3c72df7
Thobias Larsen <Thobias.Larsen@proton.me>
2025-07-25 21:04:43 +0200

    Simple refactoring and renaming

--- Mango/Mango/Interpreter.fs
+++ Mango/Mango/Interpreter.fs
@@ -5,46 +5,37 @@ open Core.Types
 open AbSyn
 open AvaloniaHelpers.AvaloniaCommonHelpers
 
-let rec temp (elements : UIElement list) (tab : TreeEnv) : UIElement list * TreeEnv =
+let rec storeElementsMarkedWithId (elements : UIElement list) (tab : TreeEnv) : UIElement list * TreeEnv =
     List.fold (fun (accElements, accTab) element ->
         match element with
         | Button(label, propsOpt, pos) ->
-            if hasProp (Option.defaultValue [] propsOpt) (function
-                | Id _ -> Some true
-                | _ -> None) then
-                let buttonId = getId (Option.defaultValue [] propsOpt)
-                match buttonId with
-                | Some id ->
-                    let tab' = SymTab.bind id (Button (label, propsOpt, pos)) accTab
-                    (accElements @ [Identifier (id, pos)], tab')
-                | None -> failwith "Expected button to have an ID"
-            else
-                (accElements @ [element], accTab)
+            let buttonId = getId (Option.defaultValue [] propsOpt)
+            match buttonId with
+            | Some id ->
+                let tab' = SymTab.bind id element tab
+                (accElements @ [Identifier (id, pos)], tab')
+            | None -> 
+                (accElements @ [element], tab)
         | TextBlock(label, commonPropsOpt, propsOpt, pos) ->
-            if hasProp (Option.defaultValue [] commonPropsOpt) (function
-                | Id _ -> Some true
-                | _ -> None) then
-                let textBlockId = getId (Option.defaultValue [] commonPropsOpt)
-                match textBlockId with
-                | Some id ->
-                    let tab' = SymTab.bind id (TextBlock (label, commonPropsOpt, propsOpt, pos)) accTab
-                    (accElements @ [Identifier (id, pos)], tab')
-                | None -> failwith "Expected TextBlock to have an ID"
-            else
-                (accElements @ [element], accTab)
+            let textId = getId (Option.defaultValue [] commonPropsOpt)
+            match textId with
+            | Some id ->
+                let tab' = SymTab.bind id element tab
+                (accElements @ [Identifier (id, pos)], tab')
+            | None -> 
+                (accElements @ [element], tab)
         | Row (commonPropsOpt, containerPropsOpt, elements, pos) ->
             let rowId = getId (Option.defaultValue [] commonPropsOpt)
-            let subElements, tab' = temp elements accTab
+            let subElements, tab' = storeElementsMarkedWithId elements accTab
             match rowId with
             | Some id ->
                 let tab'' = SymTab.bind id (Row (commonPropsOpt, containerPropsOpt, subElements, pos)) tab'
                 (accElements @ [Identifier (id, pos)], tab'')
             | None -> 
-                
                 (accElements @ subElements, tab')
         | Column (commonPropsOpt, containerPropsOpt, elements, pos) ->
             let columnId = getId (Option.defaultValue [] commonPropsOpt)
-            let subElements, tab' = temp elements accTab
+            let subElements, tab' = storeElementsMarkedWithId elements accTab
             match columnId with
             | Some id ->
                 let tab'' = SymTab.bind id (Column (commonPropsOpt, containerPropsOpt, subElements, pos)) tab'
@@ -52,7 +43,6 @@ let rec temp (elements : UIElement list) (tab : TreeEnv) : UIElement list * Tree
             | None -> 
                 (accElements @ subElements, tab')
         | _ -> 
-            // Handle other UIElement types similarly
             (accElements @ [element], accTab)
     ) ([], tab) elements
 
@@ -61,7 +51,7 @@ let rec interpret (window: HostWindow) program (tab : TreeEnv) : HostWindow =
     | AbSyn.Window (name,width, height, icon, elements, _, _) ->
         do printfn "elements: %A" elements
         do printfn "tab: %A" tab
-        let elements', tab' = temp elements tab
+        let elements', tab' = storeElementsMarkedWithId elements tab
         do printfn "elements': %A" elements'
         do printfn "tab': %A" tab'
         window |>
